b7793c642e945f891aa19cf535177007b9017715
Tadeusz Raszka <tadeuszraszka@Daniel-MacBook-Pro.local>
2018-07-04 15:20:24 +0200

    Improve BreweriesRequest and LoginRequest

--- Pivovary/Pivovary/Pivovary/Core/APIRequest/BreweriesRequest.swift
+++ Pivovary/Pivovary/Pivovary/Core/APIRequest/BreweriesRequest.swift
@@ -14,13 +14,21 @@ class BreweriesRequest {
     private let urlString = "https:www.reintodev.cz:4006/api/breweries?userId=1&skip=1"
     private let headers = ["Content-Type" : "application/json"]
     private let method: HTTPMethod = .get
+    var nameArray = [String]()
     
     
     func apiRequest() {
-        Alamofire.request(urlString, method: method, encoding: JSONEncoding.default, headers: headers).responseJSON { (response) in
+        Alamofire.request(urlString, method: method, encoding: JSONEncoding.default, headers: headers).validate().responseJSON { (response) in
             if let value = response.result.value {
+                if let json = value as? [String : Any], let name = json["name"] as? String {
+                    self.nameArray.append(name)
+                }
                 print(value)
             }
         }
+    }
+    
+    func breweriesNameArray(array: [String]) {
+        nameArray = array
     }
 }
